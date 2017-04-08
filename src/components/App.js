7c22dfe4faa45afa21414cd44d45b996ab652c9a
Annie Ritch <ritchaj@gmail.com>
2017-04-08 09:56:53 -0700

    added react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 
-
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>React/Redux Starter App</h1>
-				<p>Get started by modifying the contents of '/src/components/App.js'</p>
+				<p>Get started by modifying the contents of '/src/components/Home.js'</p>
 			</div>
 		);
 	}
 }
 
-export default App
+export default App;
